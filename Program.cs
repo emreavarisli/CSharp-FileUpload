9730a601b0f8f6b22aa98bf898a6bb6743e75abc
Hasan Emre AVARİSLİ <emreavarisli@gmail.com>
2024-04-30 12:40:56 +0300

    Program.cs de değişiklikler yapıldı.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 using FileUploadTutorial.FileUploadService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddScoped<IFileUploadService, LocalFileUploadService>();
 
 var app = builder.Build();
 
@@ -13,12 +18,6 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
-
-void ConfigureServices(IServiceCollection services)
-{
-    services.AddRazorPages();
-    services.AddScoped<IFileUploadService, LocalFileUploadService>();
 }
 
 app.UseHttpsRedirection();
